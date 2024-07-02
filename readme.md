12916e0653158febffd9c5fd5146aefc9b98cbc9
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-02 13:25:54 -0300

    Configuração do ambiente

---
+++ readme.md
@@ -0,0 +1,6 @@
+#Projeto to do list realizado para aprender typescript:
+#Tópicos:
+- Typescript;
+- React;
+- Javascript;
+- frameworks;
---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>To-do-react</h1>
+    </div>
+  );
+}
+
+export default App;
